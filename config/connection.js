bf48ba6f79f9759ba6295b806a38a9a878169924
mccaffertycr <mccaffertycr@gmail.com>
2018-08-11 00:21:26 -0400

    dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,4 @@
 var mysql = require("mysql");
-
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "todo_db"
-});
-
 var connection;
 
 // heroku / localhost conditional
@@ -15,10 +6,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
+        host: process.env.DB_HOST,
         port: 3306,
-        user: "root",
-        password: "root",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         database: "todo_db"
     });      
 }
