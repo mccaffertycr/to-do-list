6532a698fa0f153c2688faad1ef67d088d31b23c
mccaffertycr <mccaffertycr@gmail.com>
2018-08-11 01:06:56 -0400

    working

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: process.env.DB_HOST,
+        host: 'localhost',
         port: 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
+        user: 'root',
+        password: 'root',
         database: "todo_db"
     });      
 }
